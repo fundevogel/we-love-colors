a19d6a1d429b331c9062b9b989fc0be236866efa
M. Folkers <hello@twobrain.io>
2019-05-04 19:56:57 +0200

    DRYing up examples :metal:

--- examples.py
+++ examples.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import glob, os
+##
+# Imports
+# For more information, see https://www.python.org/dev/peps/pep-0008/#imports
+##
+
+import os
+import glob
+
 
 for file in glob.glob('./examples/*/index.php'):
     html = file.replace('.php', '.html')
     os.system('cd ' + os.path.dirname(file) + ' && php ' + os.path.basename(file) + ' > ' + os.path.basename(html))
+
+    print('Generating ' + html + '.. done')
