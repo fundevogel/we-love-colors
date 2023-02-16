e5ed24cbf135a02a41825c73ad6ebd68b344106a
Martin Folkers <hello@twobrain.io>
2023-02-16 19:05:18 +0100

    Fix 'RAL' colors
    This commit also deprecates 'Pillow' as dependency & fixes #25

--- src/farben/palettes/ral.py
+++ src/farben/palettes/ral.py
@@ -3,12 +3,10 @@ This module is part of the 'farben' package,
 which is released under MIT license.
 """
 
-from urllib.request import urlopen
-
-from PIL import ImageFile
+import re
 
 from ..palette import Palette
-from ..utils import rgb2hex
+from ..utils import hex2rgb
 
 
 class RAL(Palette):
@@ -20,18 +18,7 @@ class RAL(Palette):
     identifier = "ral"
 
     # Dictionary holding fetched colors
-    sets = {
-        "classic": [],
-        "design": [],
-        "effect": [],
-        "plastics-p1": [],
-        "plastics-p2": [],
-        # IDEA: Merging them together?
-        # 'plastics' {
-        #   'p1': [],
-        #   'p2': [],
-        # }
-    }
+    sets = {"ral": []}
 
     # Copyright notices
     copyright_notices = {
@@ -52,82 +39,25 @@ class RAL(Palette):
         :return: None
         """
 
-        self.fetch("classic")
-        self.fetch("design")
-        self.fetch("effect")
-        self.fetch("plastics")
-
-    def fetch(self, set_name: str) -> None:
-        """
-        Fetches RAL® colors
-
-        Available sets:
-          - 'classic'
-          - 'design'
-          - 'effect'
-          - 'plastics
-
-        :param set_name: str Name of color set
-        :return: None
-        """
-
         # One URL to rule them all
-        base_url = (
-            "https://www.ral-farben.de/content/anwendung-hilfe/"
-            + f"all-ral-colours-names/overview-ral-{set_name}-colours.html"
-        )
+        base_url = "https://www.ral-farben.de/alle-ral-farben"
 
         # Scrape RAL® colors from HTML
         soup = self.get_html(base_url)
-        color_grids = soup.find_all("ul", {"class": "color-grid"})
 
-        for index, color_grid in enumerate(color_grids, 1):
-            list_elements = color_grid.findAll("li")
+        for color_tile in soup.find_all("a", {"class": "farbe"}):
+            code = color_tile.find("span", {"class": "number"}).text
+            hexa = color_tile["style"].split(":")[1]
 
-            for idx, list_element in enumerate(list_elements, 1):
-                list_element = list_element.text.splitlines()
+            color = {}
+            color["code"] = f'{color_tile.find("span").text} {code}'
+            color["rgb"] = f"rgb({hex2rgb(hexa)})"
+            color["hex"] = hexa.upper()
+            color["name"] = ""
 
-                # Parsing each RAL® color's background image, extracting RGB values
-                slug = {"design": "designplus", "effect": "ral-effect"}
+            if subtext := color_tile.find("div", {"class": "subtext"}):
+                color["name"] = re.split("\n", subtext.get_text(separator="\n"))[1]
 
-                identifier = slug.get(set_name) if (set_name in slug) else set_name
+            self.sets["ral"].append(color)
 
-                if set_name == "plastics":
-                    identifier = set_name + "-p" + str(index)
-
-                # See https://stackoverflow.com/a/2271015
-                image_url = (
-                    "https://www.ral-farben.de/out/ralfarben/img/thumbs/"
-                    + f"{identifier}-{idx}.png"
-                )
-                image = urlopen(image_url)
-                parser = ImageFile.Parser()
-
-                while True:
-                    chunk = image.read(1024)
-
-                    if not chunk:
-                        break
-
-                    parser.feed(chunk)
-
-                image = parser.close()
-                rgb_values = [str(i) for i in image.getpixel((0, 0))]
-
-                color = {}
-                color["code"] = list_element[1].strip()
-                color["rgb"] = "rgb(" + ",".join(rgb_values) + ")"
-                color["hex"] = rgb2hex(rgb_values)
-                color["name"] = ""
-
-                if len(list_element) > 2:
-                    color["name"] = (
-                        list_element[2] if len(list_element) == 3 else list_element[3]
-                    )
-
-                if not set_name == "plastics":
-                    self.sets[set_name].append(color)
-                else:
-                    self.sets[identifier].append(color)
-
-                print(f'Loading {color["code"]} in set "{set_name}" .. done')
+            print(f'Loading {color["code"]} in set "ral" .. done')
